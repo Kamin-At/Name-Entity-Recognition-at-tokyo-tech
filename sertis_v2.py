64b18b0a53940c21477f8edfeb32b20e2817ec5e
min <kamin.atsavasirilert>
2019-10-02 23:22:08 +0900

    Fix sertis_v2

--- sertis_v2.py
+++ sertis_v2.py
@@ -13,11 +13,9 @@ def nonzero(a):
 def split(s, indices):
     return [s[i:j] for i,j in zip(indices, indices[1:]+[None])]
 
-def sertis_tokenizer(text, ind, saved_model_path):
-    if ind%2 == 1:
-        return (-1,-1,-1)
+def sertis_tokenizer(text, saved_model_path):
     text = text.strip().split('||')
-    print(text)
+    #print(text)
     for i in range(len(text)):
         if text[i] == '':
             text[i] = ' '
@@ -36,8 +34,8 @@ def sertis_tokenizer(text, ind, saved_model_path):
         label = []
         all_words = []
         for i, j in enumerate(inputs):
-            if j == [ThaiWordSegmentLabeller.get_input_labels('')]:
-                print('YES')
+            # if j == [ThaiWordSegmentLabeller.get_input_labels('')]:
+                # print('YES')
             #print(j)
             #print(lengths[i])
             y = session.run(g_outputs, feed_dict = {g_inputs: j, g_lengths: lengths[i], g_training: False})
@@ -48,56 +46,94 @@ def sertis_tokenizer(text, ind, saved_model_path):
             else:
                 label = label + ['0']*len(words)
             all_words = all_words + words
-    #print(words)
+    print(len(words))
     #print(all_words)
     #print(label)
-    print('finished!!!')
+    #print('finished!!!')
     return ('||'.join(all_words), '||'.join(label), len(label))
 
 def main():
     os.chdir('E:\\min\\Tokyo_tech_exchange\\NER\\data')
     saved_model_path = 'E:\\Coding_projects\\Name-Entity-Recognition-at-tokyo-tech\\saved_model'
     num_process = os.cpu_count()
-    
+    total_num_line = {} 
+    for i in [ 'dat', 'loc', 'per', 'oth']:
+        with open(i+'.txt', 'r', encoding='utf8') as f:
+            num_line = 0
+            for line in f:
+                num_line = num_line + 1
+            total_num_line[i] = num_line
     print(num_process)
-    with mp.Pool(processes=num_process) as pool:
-        all_types = ['mea', 'org', 'dat', 'loc', 'per', 'oth']
-        all_word_cnt = []
-        all_article_cnt = []
-        all_max = []
-        all_min = []
-        for cur_type in all_types:
-            print(cur_type)
-            print('now reach: ' + str((all_types.index(cur_type)+1)/len(all_types)))
-            cnt_word = 0
-            cnt_sentence = 0
-            word_min = 100000000
-            word_max = 0
-            with open(cur_type+'.txt', 'r', encoding='utf8') as f:
-                with open('.\\sertis_data\\'+ cur_type + '_sertis.txt', 'a', encoding = 'utf8') as f_out:
-                    with open('.\\sertis_data\\'+ cur_type + '_label_sertis.txt', 'a', encoding = 'utf8') as f_out_label:
-                        results = [pool.apply(sertis_tokenizer, args=(re.sub(r'[^ก-๙A-Za-z0-9.-/%:]','  ',x),ind,saved_model_path)) for ind, x in enumerate(f)]
-                        for Words, Label, num_word in results:
-                            if Words == -1:
-                                continue
-                            cnt_sentence = cnt_sentence + 1
-                            cnt_word = cnt_word + num_word
-                            word_min = min(word_min,num_word)
-                            word_max = max(word_max,num_word)
-                            f_out.write(Words)
-                            f_out.write('\n')
-                            f_out_label.write(Label)
-                            f_out_label.write('\n')
-                        all_word_cnt.append(cnt_word)
-                        all_article_cnt.append(cnt_sentence)
-                        all_max.append(word_max)
-                        all_min.append(word_min)
-        with open('.\\sertis_data\\report_sertis.txt', 'w') as f:
-            f.write('all_type: ' + ','.join(all_types) + '\n')
-            f.write('word_cnt: '+ ','.join(all_word_cnt) + '\n')
-            f.write('article_cnt: '+ ','.join(all_article_cnt)+'\n')
-            f.write('max: '+ ','.join(all_max) + '\n')
-            f.write('min: '+ ','.join(all_min)+'\n')
+    all_types = ['dat', 'loc', 'per', 'oth']
+    all_word_cnt = []
+    all_article_cnt = []
+    all_max = []
+    all_min = []
+    batch_size = 300
+    for cur_type in all_types:
+        print(cur_type)
+        print('now reach: ' + str((all_types.index(cur_type)+1)/len(all_types)))
+        cnt_word = 0
+        cnt_sentence = 0
+        word_min = 100000000
+        word_max = 0
+        with open(cur_type+'.txt', 'r', encoding='utf8') as f:
+            article = []
+            last_batch = 0
+            for line_ind, line in enumerate(f):
+                if line_ind%2==0:
+                    article.append(line)
+                    print('reach: ' + str(line_ind) + 'from' + str(total_num_line[cur_type]))
+                    if line_ind % batch_size == 0:
+                        if line_ind !=0:
+                            pool = mp.Pool(processes=num_process)
+                            results = [pool.apply(sertis_tokenizer, args=(re.sub(r'[^ก-๙A-Za-z0-9.-/%:]','  ',x),saved_model_path)) for x in article]
+                            pool.close()
+                            pool.join()
+                            with open('.\\sertis_data\\'+ cur_type + '_sertis.txt', 'a', encoding = 'utf8') as f_out:
+                                with open('.\\sertis_data\\'+ cur_type + '_label_sertis.txt', 'a', encoding = 'utf8') as f_out_label:
+                                    for Words, Label, num_word in results:
+                                        if Words == -1:
+                                            continue
+                                        cnt_sentence = cnt_sentence + 1
+                                        cnt_word = cnt_word + num_word
+                                        word_min = min(word_min,num_word)
+                                        word_max = max(word_max,num_word)
+                                        f_out.write(Words)
+                                        f_out.write('\n')
+                                        f_out_label.write(Label)
+                                        f_out_label.write('\n')
+                                    article = []
+            pool = mp.Pool(processes=num_process)
+            results = [pool.apply(sertis_tokenizer, args=(re.sub(r'[^ก-๙A-Za-z0-9.-/%:]','  ',x),saved_model_path)) for x in article]
+            pool.close()
+            pool.join()
+            with open('.\\sertis_data\\'+ cur_type + '_sertis.txt', 'a', encoding = 'utf8') as f_out:
+                with open('.\\sertis_data\\'+ cur_type + '_label_sertis.txt', 'a', encoding = 'utf8') as f_out_label:
+                    for Words, Label, num_word in results:
+                        if Words == -1:
+                            continue
+                        cnt_sentence = cnt_sentence + 1
+                        cnt_word = cnt_word + num_word
+                        word_min = min(word_min,num_word)
+                        word_max = max(word_max,num_word)
+                        f_out.write(Words)
+                        f_out.write('\n')
+                        f_out_label.write(Label)
+                        f_out_label.write('\n')
+                    article = []            
+                    
+        all_word_cnt.append(cnt_word)
+        all_article_cnt.append(cnt_sentence)
+        all_max.append(word_max)
+        all_min.append(word_min)
+        
+    with open('.\\sertis_data\\report_sertis.txt', 'w') as f:
+        f.write('all_type: ' + ','.join(all_types) + '\n')
+        f.write('word_cnt: '+ ','.join(all_word_cnt) + '\n')
+        f.write('article_cnt: '+ ','.join(all_article_cnt)+'\n')
+        f.write('max: '+ ','.join(all_max) + '\n')
+        f.write('min: '+ ','.join(all_min)+'\n')
 
 if __name__ == '__main__':
     main()
